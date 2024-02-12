a1bb0231964fcfed3be758a92e472cca2227d71e
Saifullakh1 <saifullakh7@gmail.com>
2024-02-12 10:33:23 +0600

    created new func

--- apps/tours/urls.py
+++ apps/tours/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('tour/<int:id>/', views.tour_detail, name='tour-detail')
 ]
--- apps/tours/views.py
+++ apps/tours/views.py
@@ -5,3 +5,9 @@ from .models import Tour
 def index(request):
     tours = Tour.objects.all()
     return render(request, 'tours/tour_list.html', {'tours': tours})
+
+
+def tour_detail(request, id):
+    tour = Tour.objects.get(id=id)
+    return render(request, 'tours/tour_detail.html', {'tour': tour})
+
